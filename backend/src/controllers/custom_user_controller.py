b277253028186ea36630323fb32f71952646acfd
thdecamargoj <thdecamargoj@gmail.com>
2024-09-22 02:19:03 -0300

    Implementação da deleção de usuário

--- backend/src/controllers/custom_user_controller.py
+++ backend/src/controllers/custom_user_controller.py
@@ -1,25 +1,43 @@
 import json
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.forms import ValidationError
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
 from django.views import View
-from src.services.custom_user_service import create_custom_user
+from src.exceptions.response_exceptions import (
+    EntityNotFound,
+    InternalServerError,
+    UnprocessableEntity,
+)
+from src.services.custom_user_service import create_custom_user, delete_custom_user
 
 
 class UserController(View):
-    def get(self, request):
-        pass
-
-    def post(self, request):
+    def get(self, request: HttpRequest, user_id: int):
         try:
-            return JsonResponse(
-                create_custom_user(json.loads(request.body)), status=200
-            )
+            pass
+        except Exception as e:
+            return InternalServerError()
+
+    def post(self, request: HttpRequest):
+        try:
+            return JsonResponse(create_custom_user(json.loads(request.body)), status=200)
         except ValidationError as e:
-            return JsonResponse({"error": e.message}, status=500)
+            return UnprocessableEntity()
+        except Exception as e:
+            return InternalServerError()
 
-    def delete(self, request):
-        pass
+    def delete(self, request: HttpRequest, user_id: int):
+        try:
+            delete_custom_user(user_id)
+            return JsonResponse({}, status=204)
+        except ObjectDoesNotExist as e:
+            return EntityNotFound()
+        except Exception as e:
+            return InternalServerError()
 
-    def put(self, request):
-        pass
+    def put(self, request: HttpRequest):
+        try:
+            pass
+        except Exception as e:
+            return InternalServerError()
--- backend/src/tests/test_user_service.py
+++ backend/src/tests/test_user_service.py
@@ -1,60 +1,74 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
-from src.models.custom_user import (
-    CustomUser,
-    CustomUserSerializer,
-    UserSerializer,
-)
-from src.services.custom_user_service import create_custom_user
+from src.models.custom_user import CustomUser
+from src.services.custom_user_service import create_custom_user, delete_custom_user
+
+VALID_USER_INPUT_MOCK = {
+    "username": "testuser",
+    "password": "TestPassword123",
+    "email": "testuser@example.com",
+}
+
+INVALID_USER_INPUT_MOCKS = [
+    # sem '.' no email
+    {
+        "username": "testuser",
+        "password": "TestPassword123",
+        "email": "testuser@example",
+    },
+    # sem '@' no email
+    {
+        "username": "testuser2",
+        "password": "TestPassword123",
+        "email": "testuserexample.com",
+    },
+    # username ja existente
+    {
+        "username": "testuser",
+        "password": "TestPassword123",
+        "email": "testuser@example.com",
+    },
+]
 
 
-class CreateCustomUserTestCase(TestCase):
-    def setUp(self):
-        self.valid_user_data = {
-            "username": "testuser",
-            "password": "TestPassword123",
-            "email": "testuser@example.com",
-        }
-        self.invalid_data = [
-            # sem '.' no email
-            {
-                "username": "testuser",
-                "password": "TestPassword123",
-                "email": "testuser@example",
-            },
-            # sem '@' no email
-            {
-                "username": "testuser2",
-                "password": "TestPassword123",
-                "email": "testuserexample.com",
-            },
-            # username ja existente
-            {
-                "username": "testuser",
-                "password": "TestPassword123",
-                "email": "testuser@example.com",
-            },
-        ]
-
+class TestCreateCustomUserService(TestCase):
     def test_create_custom_user_valid_data(self):
         # Teste para usuario valido
-        result = create_custom_user(self.valid_user_data)
+        result = create_custom_user(VALID_USER_INPUT_MOCK)
         # Pega usuario no BD
-        user = User.objects.get(username="testuser")
+        user = User.objects.get(username=VALID_USER_INPUT_MOCK["username"])
         custom_user = CustomUser.objects.get(user=user)
         # Valida retorno da funcao
         self.assertEqual(result["id"], 1)
-        self.assertEqual(result["user"]["email"], "testuser@example.com")
+        self.assertEqual(result["user"]["email"], VALID_USER_INPUT_MOCK["email"])
         # Checa se usuario foi salvo corretamente
-        self.assertEqual(user.email, "testuser@example.com")
-        self.assertTrue(user.check_password("TestPassword123"))
+        self.assertEqual(user.email, VALID_USER_INPUT_MOCK["email"])
+        self.assertTrue(user.check_password(VALID_USER_INPUT_MOCK["password"]))
         self.assertEqual(custom_user.user, user)
 
     def test_create_custom_user_invalid_data(self):
         # Teste para usuario invalido
-        create_custom_user(
-            self.valid_user_data
-        )  # cria usuario com username = 'testuser'
-        for data in self.invalid_data:
+
+        # cria usuario com username = 'testuser'
+        create_custom_user(VALID_USER_INPUT_MOCK)
+        for data in INVALID_USER_INPUT_MOCKS:
             with self.assertRaises(Exception):
                 create_custom_user(data)
+
+
+class TestDeleteCustomUserService(TestCase):
+    def setUp(self) -> None:
+        create_custom_user(VALID_USER_INPUT_MOCK)
+
+    def tearDown(self) -> None:
+        if len(User.objects.filter(id=1)):
+            User.objects.filter(id=1).delete()
+
+    def test_custom_user_not_found(self):
+        with self.assertRaises(ObjectDoesNotExist):
+            delete_custom_user(0)
+
+    def test_custom_user_deleted_successfully(self):
+        delete_custom_user(1)
+        assert len(User.objects.filter(id=1)) == 0

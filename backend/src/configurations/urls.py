29ca4d942826b1c77b7e29653154b07f61fc5115
Pedro Brasil Barroso <p260637@dac.unicamp.br>
2024-11-09 18:31:53 -0300

    create domain table (#48)

--- backend/src/configurations/urls.py
+++ backend/src/configurations/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.permissions import AllowAny
 from src.controllers.custom_user_controller import UserController
+from src.controllers.domain_controller import DomainController
 from src.controllers.login_controller import LoginView
 from src.controllers.logout_controller import LogoutView
 
@@ -15,6 +16,8 @@ urlpatterns = [
         "user/<int:user_id>/",
         UserController.as_view(http_method_names=["put", "get", "delete"]),
     ),
+    # Domain endpoints
+    path("domain/<int:domain_id>/", DomainController.as_view(http_method_names=["get"])),
     # Authentication endpoints
     path("auth/login/", LoginView.as_view()),
     path("auth/logout/", LogoutView.as_view()),
